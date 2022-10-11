8c16053cbcc3c2b45e34cf6ad6c0109a5be77c62
samdejong <97760559+samdejong@users.noreply.github.com>
2022-10-11 16:35:32 -0400

    lab5_p3

---
+++ lab/EGR326_Lab5_Part03/Debug/subdir_vars.mk
@@ -0,0 +1,71 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../I2C.c \
+../Read_Keypad.c \
+../SysTickTimer.c \
+../main.c \
+../pinsInint.c \
+../readInputs.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./I2C.d \
+./Read_Keypad.d \
+./SysTickTimer.d \
+./main.d \
+./pinsInint.d \
+./readInputs.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./I2C.obj \
+./Read_Keypad.obj \
+./SysTickTimer.obj \
+./main.obj \
+./pinsInint.obj \
+./readInputs.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"I2C.obj" \
+"Read_Keypad.obj" \
+"SysTickTimer.obj" \
+"main.obj" \
+"pinsInint.obj" \
+"readInputs.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"I2C.d" \
+"Read_Keypad.d" \
+"SysTickTimer.d" \
+"main.d" \
+"pinsInint.d" \
+"readInputs.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../I2C.c" \
+"../Read_Keypad.c" \
+"../SysTickTimer.c" \
+"../main.c" \
+"../pinsInint.c" \
+"../readInputs.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
