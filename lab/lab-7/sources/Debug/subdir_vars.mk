7925eb3a0e0459500cb520db227139296c9e3fbb
Jack-Lukomski <jtlukomski@gmail.com>
2022-10-14 15:58:16 -0400

    Started lab 7

---
+++ lab/lab-7/sources/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../Toggle.c \
+../main.c \
+../pinsInint.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Toggle.d \
+./main.d \
+./pinsInint.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Toggle.obj \
+./main.obj \
+./pinsInint.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Toggle.obj" \
+"main.obj" \
+"pinsInint.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Toggle.d" \
+"main.d" \
+"pinsInint.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../Toggle.c" \
+"../main.c" \
+"../pinsInint.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
---
+++ lab/lab-7/sources/main.c
@@ -0,0 +1,32 @@
+#include "msp.h"
+#include "pinsInint.h"
+#include "Toggle.h"
+
+port2GPIO_t motor_a;
+port2GPIO_t motor_b;
+port2GPIO_t motor_ap;
+port2GPIO_t motor_bp;
+
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	vpinsInit_GPIO(&motor_a, pin7, output, pullup, disableInterrupt);
+	vpinsInit_GPIO(&motor_b, pin6, output, pullup, disableInterrupt);
+	vpinsInit_GPIO(&motor_ap, pin5, output, pullup, disableInterrupt);
+	vpinsInit_GPIO(&motor_bp, pin4, output, pullup, disableInterrupt);
+
+
+	while(1)
+	{
+	    vToggle_turnPinLow(motor_b);
+	    vToggle_turnPinLow(motor_bp);
+	    vToggle_turnPinHigh(motor_a);
+	    vToggle_turnPinHigh(motor_ap);
+	    __delay_cycles(30000);
+        vToggle_turnPinLow(motor_a);
+        vToggle_turnPinLow(motor_ap);
+        vToggle_turnPinHigh(motor_b);
+        vToggle_turnPinHigh(motor_bp);
+        __delay_cycles(30000);
+	}
+}
